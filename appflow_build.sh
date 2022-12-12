14a6bba5a18a31dcee6b709c61b44d63a38cd1e0
jon <jon.herman20@gmail.com>
2022-12-12 16:30:58 -0500

    execute build script with bash

--- appflow_build.sh
+++ appflow_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $CI_APP_ID == "test1234" ]; then
+if [[ $CI_APP_ID == "test1234" ]]; then
     #If that check is true we know the build is running in appflow
     echo "this is appflow"
 fi
